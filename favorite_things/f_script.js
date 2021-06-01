b7950a129ade757612839a201883a61a4e031deb
maybeluna <ksb981703@gmail.com>
2021-06-02 02:17:06 +0900

    junes

---
+++ favorite_things/f_script.js
@@ -0,0 +1,11 @@
+var menuHeight = document.querySelector("nev").offsetHeight;
+var movie_location = document.querySelector("#movie").offsetTop;
+window.scrollTo({top: movie_location});
+
+var menuHeight = document.querySelector("nev").offsetHeight;
+var book_location = document.querySelector("#book").offsetTop;
+window.scrollTo({top: book_location});
+
+var menuHeight = document.querySelector("nev").offsetHeight;
+var playlist_location = document.querySelector("#playlist").offsetTop;
+window.scrollTo(0, playlist_location};
